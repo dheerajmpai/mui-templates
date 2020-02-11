f95cbaa46b17e4b475359c659bb3addcecc772b7
Dheeraj M Pai <29203967+dheerajmpai@users.noreply.github.com>
2020-02-12 01:49:53 +0530

    file change

--- pages/index.js
+++ pages/index.js
@@ -12,13 +12,9 @@ export default function App() {
 
         <div className="content" dangerouslySetInnerHTML={{ __html: thisIsMyCopy2 }}></div>
         <div className="content" dangerouslySetInnerHTML={{ __html: thisIsMyCopy3 }}></div>
-        <Container>
+        <Container style={{margin : '20px 20px 20px 20px auto'}}>
             <Header />
-            {/* <blockquote class="twitter-tweet"><p lang="en" dir="ltr">I will open Free self Defense Coaching Centre for Girls in Hari Nagar Vidhanasabha in next 30 Days. It was my promise in my manifesto, I lost but i will try my best to fullfil my Promise to my people on my own Capacity</p>&mdash; Tajinder Pal Singh Bagga (@TajinderBagga) <a href="https://twitter.com/TajinderBagga/status/1227191554852315136?ref_src=twsrc%5Etfw">February 11, 2020</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> */}
             <GridPage />
-            <div className="content" dangerouslySetInnerHTML={{ __html: thisIsMyCopy }}></div>
-
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/ZS3O0OOn0a0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </Container>
     </>)
 
