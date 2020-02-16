8bebfb3872c1f910b07c2e0d77baa88ef67839f1
Dheeraj M Pai <29203967+dheerajmpai@users.noreply.github.com>
2020-02-16 11:33:59 +0530

    file change

--- constants/global.constants.js
+++ constants/global.constants.js
@@ -7,6 +7,8 @@ export const WEBSITE_URL = "https://hyperweb.ai";
 export const BACKEND_URL = "https://api.hyperweb.ai/v1/";
 export const SUPPORT_EMAIL = "editor@sae.news";
 
+
+
 export const COLOR_1 = '#142850';
 export const COLOR_2 = '#27496d';
 export const COLOR_3 = '#0c7b93';
